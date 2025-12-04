f8a97c144b531d090db70095a714b0fc7aa42f14
Neas93 <Neas93@gmail.com>
2025-12-04 12:21:43 +0100

    Implement temperature and humidity reading methods

--- Arduino/Datalogger_sketch/TempSensor2.cpp
+++ Arduino/Datalogger_sketch/TempSensor2.cpp
@@ -1,1 +1,18 @@
+#include "TempSensor2.h"
+
+TempSensor::TempSensor(int pin) : sensor(pin) { }
+
+
+float TempSensor::readTemperature() 
+{
+    sensor.read(); 
+    return sensor.getTemperature();
+}
+
+
+float TempSensor::readHumidity()
+{
+    sensor.read();
+    return sensor.getHumidity();
+}
 
