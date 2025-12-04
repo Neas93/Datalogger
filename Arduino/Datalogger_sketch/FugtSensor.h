27dc2f89a7c9d712051dc4407667d84029d744bc
Neas93 <Neas93@gmail.com>
2025-12-04 12:16:18 +0100

    Update FugtSensor.h

--- Arduino/Datalogger_sketch/FugtSensor.h
+++ Arduino/Datalogger_sketch/FugtSensor.h
@@ -1,1 +1,17 @@
+#include "TempSensor.h"
 
+TempSensor::TempSensor(int pin) : sensor(pin) { }
+
+
+float TempSensor::readTemperature() 
+{
+    sensor.read(); 
+    return sensor.getTemperature();
+}
+
+
+float TempSensor::readHumidity()
+{
+    sensor.read();
+    return sensor.getHumidity();
+}
