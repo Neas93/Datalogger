2618dc18a860bb08c053fa026e585f52fb6eda51
Neas93 <Neas93@gmail.com>
2025-12-04 12:16:08 +0100

    Update TempSensor.h

--- Arduino/Datalogger_sketch/TempSensor.h
+++ Arduino/Datalogger_sketch/TempSensor.h
@@ -1,1 +1,14 @@
+#pragma once
+#include <Arduino.h>
+#include <dhtnew.h>
 
+class TempSensor 
+{
+public:
+    DHTNEW sensor;
+
+    TempSensor(int pin);
+
+    float readTemperature();
+    float readHumidity();
+};
