d563fa1dc6ba42f3e076536e7682a6f1a5b04fb3
Neas93 <Neas93@gmail.com>
2025-12-04 20:17:29 +0100

    Update Datalogger_sketch.ino

--- Arduino/Datalogger_sketch/Datalogger_sketch.ino
+++ Arduino/Datalogger_sketch/Datalogger_sketch.ino
@@ -1,44 +1,57 @@
 #include "TempSensor1.h"
 #include "TempSensor2.h"
-
+#include "SD_log.h"
 
 TempSensor1 sensor1(7);
 TempSensor2 sensor2(6);
+SD_log sdlog(53, "Hive_Log.txt");
 
 void setup()
 {
     Serial.begin(9600);
     delay(3000);
+
+    sdlog.begin();
 }
 
 void loop()
 {
-    // Læs værdier fra Sensor 1
     float temp1 = sensor1.readTemperature();
     float hum1  = sensor1.readHumidity();
 
-    // Læs værdier fra Sensor 2
     float temp2 = sensor2.readTemperature();
     float hum2  = sensor2.readHumidity();
 
-    // Print Sensor 1
     if (temp1 > -30)
     {
-    Serial.print("Sensor 1 - Temperatur: ");
-    Serial.print(temp1, 1);
-    Serial.print(" °C, Fugtighed: ");
-    Serial.print(hum1, 1);
-    Serial.println("%");
-    }
-    // Print Sensor 2
-    if (temp2 > -30)
-    {
-    Serial.print("Sensor 2 - Temperatur: ");
-    Serial.print(temp2, 1);
-    Serial.print(" °C, Fugtighed: ");
-    Serial.print(hum2, 1);
-    Serial.println("%");
+        Serial.print("Sensor 1 - Temperatur: ");
+        Serial.print(temp1, 1);
+        Serial.print(" °C, Fugtighed: ");
+        Serial.print(hum1, 1);
+        Serial.println("%");
     }
 
-    delay(2000); 
+    if (temp2 > -30)
+    {
+        Serial.print("Sensor 2 - Temperatur: ");
+        Serial.print(temp2, 1);
+        Serial.print(" °C, Fugtighed: ");
+        Serial.print(hum2, 1);
+        Serial.println("%");
+    }
+    if (temp1 > -30 && temp2 > -30 )
+    {
+        sdlog.log(temp1, hum1, temp2, hum2);
+    }
+
+    else if (temp2 > -30 && temp1 < -30)
+    {
+        sdlog.log(NAN, NAN, temp2, hum2);
+    }
+
+    else if (temp2 < -30 && temp1 > -30)
+    {
+        sdlog.log(temp1, hum1, NAN, NAN);  
+    }
+    delay(5000); // 10 minutter
 }
