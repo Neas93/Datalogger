ce14489a22669e101a981d5406b510d0c7a9df67
Neas93 <Neas93@gmail.com>
2025-12-04 20:17:19 +0100

    Update SD_log.h

--- Arduino/Datalogger_sketch/SD_log.h
+++ Arduino/Datalogger_sketch/SD_log.h
@@ -1,1 +1,15 @@
+#pragma once
 #include <Arduino.h>
+#include <SD.h>
+
+class SD_log
+{
+public:
+    int csPin;
+    const char* fileName;
+
+    SD_log(int cs, const char* name);
+
+    void begin();
+    void log(float temp1, float hum1, float temp2, float hum2);
+};
