f25ce6e0fc58a25b0b8860184698b6e3488b1501
Neas93 <Neas93@gmail.com>
2025-12-04 20:17:24 +0100

    Update SD_log.cpp

--- Arduino/Datalogger_sketch/SD_log.cpp
+++ Arduino/Datalogger_sketch/SD_log.cpp
@@ -1,1 +1,51 @@
 #include "SD_log.h"
+
+SD_log::SD_log(int cs, const char* name)
+{
+    csPin = cs;
+    fileName = name;
+}
+
+void SD_log::begin()
+{
+    Serial.print("Indlæser SD... ");
+
+    if (!SD.begin(csPin)) {
+        Serial.println("FEJL!");
+        return;
+    }
+
+    Serial.println("OK");
+
+    File file = SD.open(fileName, FILE_WRITE);
+    if (file) {
+        file.println("Starter logføring:");
+        file.close();
+    }
+}
+
+void SD_log::log(float temp1, float hum1, float temp2, float hum2)
+{
+    File file = SD.open(fileName, FILE_WRITE);
+
+    if (!file) {
+        Serial.println("Fejl i åbning af logfil!");
+        return;
+    }
+
+    file.print("S1 Temp: ");
+    file.print(temp1, 1);
+    file.print(" C, S1 Hum: ");
+    file.print(hum1, 1);
+    file.print("% | ");
+
+    file.print("S2 Temp: ");
+    file.print(temp2, 1);
+    file.print(" C, S2 Hum: ");
+    file.print(hum2, 1);
+    file.println("%");
+
+    file.close();
+
+    Serial.println("Data indskrevet på SD");
+}
