e51454447ab8b11d4507733e29191a893d0da9e5
Neas93 <Neas93@gmail.com>
2025-12-05 10:10:29 +0100

    Update SD_log.cpp

--- Arduino/Datalogger_sketch/SD_log.cpp
+++ Arduino/Datalogger_sketch/SD_log.cpp
@@ -36,13 +36,13 @@ void SD_log::log(float temp1, float hum1, float temp2, float hum2)
 
     file.print("S1 Temp: ");
     file.print(temp1, 1);
-    file.print(" C, S1 Hum: ");
+    file.print(" C| S1 Hum: ");
     file.print(hum1, 1);
     file.print("% | ");
 
     file.print("S2 Temp: ");
     file.print(temp2, 1);
-    file.print(" C, S2 Hum: ");
+    file.print(" C| S2 Hum: ");
     file.print(hum2, 1);
     file.println("%");
 
