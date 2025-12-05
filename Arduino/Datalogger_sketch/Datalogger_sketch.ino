87b382885239cced64d34083a93bcf305295db05
Neas93 <Neas93@gmail.com>
2025-12-05 14:17:06 +0100

    Update Datalogger_sketch.ino

--- Arduino/Datalogger_sketch/Datalogger_sketch.ino
+++ Arduino/Datalogger_sketch/Datalogger_sketch.ino
@@ -1,27 +1,34 @@
 #include "TempSensor1.h"
 #include "TempSensor2.h"
 #include "SD_log.h"
+#include "RTC.h"
 
+// Opret instanser
 TempSensor1 sensor1(7);
 TempSensor2 sensor2(6);
 SD_log sdlog(53, "HiveLog1.txt");
+RTC clock1;
 
 void setup()
 {
     Serial.begin(9600);
     delay(3000);
 
+    // Start SD og RTC
     sdlog.begin();
+    clock1.begin();
 }
 
 void loop()
 {
+    // Læs sensorer
     float temp1 = sensor1.readTemperature();
     float hum1  = sensor1.readHumidity();
 
     float temp2 = sensor2.readTemperature();
     float hum2  = sensor2.readHumidity();
 
+    // Udskriv til Serial
     if (temp1 > -30)
     {
         Serial.print("Sensor 1 - Temperatur: ");
@@ -39,19 +46,26 @@ void loop()
         Serial.print(hum2, 1);
         Serial.println("%");
     }
-    if (temp1 > -30 && temp2 > -30 )
-    {
-        sdlog.log(temp1, hum1, temp2, hum2);
-    }
 
-    else if (temp1 < -30 && temp2 > -30)
-    {
-        sdlog.log(NAN, NAN, temp2, hum2);
-    }
+    // Log til SD med timestamp
+    String timestamp = clock1.getTimestamp();
 
-    else if (temp1 > -30 && temp2 < -30)
-    {
-        sdlog.log(temp1, hum1, NAN, NAN);  
-    }
-    delay(5000); // 10 minutter
+    if (temp1 > -30 && temp2 > -30)
+{
+    sdlog.log(timestamp, temp1, hum1, temp2, hum2);
+}
+else if (temp1 < -30 && temp2 > -30)
+{
+    sdlog.log(timestamp, NAN, NAN, temp2, hum2);
+}
+else if (temp1 > -30 && temp2 < -30)
+{
+    sdlog.log(timestamp, temp1, hum1, NAN, NAN);  
+}
+
+    // Tilføj timestamp i Serial (valgfrit)
+    Serial.print("Tidspunkt: ");
+    Serial.println(timestamp);
+
+    delay(5000); // 5 sekunder; ændr til 10 min = 600000 ms
 }
