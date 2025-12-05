82d8a80206fd6b82aca9533ff09d06a8c57c7d75
Neas93 <Neas93@gmail.com>
2025-12-05 14:17:00 +0100

    Update RTC.h

--- Arduino/Datalogger_sketch/RTC.h
+++ Arduino/Datalogger_sketch/RTC.h
@@ -1,1 +1,16 @@
+#pragma once
 #include <Arduino.h>
+#include <Wire.h>
+#include <DS3231.h>
+
+class RTC
+{
+public:
+    DS3231 clock;
+    RTCDateTime dt;
+
+    RTC();          
+    void begin();   
+    String getTimestamp();  
+    float getTemperature(); 
+};
