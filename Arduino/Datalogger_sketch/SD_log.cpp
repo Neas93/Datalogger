fe14dd73bc92b74df54d53f86c96188d2c61d5b0
Neas93 <Neas93@gmail.com>
2025-12-05 14:16:58 +0100

    Update SD_log.cpp

--- Arduino/Datalogger_sketch/SD_log.cpp
+++ Arduino/Datalogger_sketch/SD_log.cpp
@@ -9,44 +9,35 @@ SD_log::SD_log(int cs, const char* name)
 void SD_log::begin()
 {
     Serial.print("Indlæser SD... ");
-
     if (!SD.begin(csPin)) {
         Serial.println("FEJL!");
         return;
     }
-
     Serial.println("OK");
 
     File file = SD.open(fileName, FILE_WRITE);
     if (file) {
         file.println("Starter logføring:");
-        file.println("Ved fejl i måling skrives NAN");
+        file.println("Format: Timestamp | S1 Temp | S1 Hum | S2 Temp | S2 Hum");
         file.close();
     }
 }
 
-void SD_log::log(float temp1, float hum1, float temp2, float hum2)
+void SD_log::log(String timestamp, float temp1, float hum1, float temp2, float hum2)
 {
     File file = SD.open(fileName, FILE_WRITE);
-
     if (!file) {
         Serial.println("Fejl i åbning af logfil!");
         return;
     }
 
-    file.print("S1 Temp: ");
-    file.print(temp1, 1);
-    file.print(" C| S1 Hum: ");
-    file.print(hum1, 1);
-    file.print("% | ");
-
-    file.print("S2 Temp: ");
-    file.print(temp2, 1);
-    file.print(" C| S2 Hum: ");
-    file.print(hum2, 1);
+    file.print(timestamp);
+    file.print(" | S1 Temp: "); file.print(temp1, 1);
+    file.print(" C | S1 Hum: "); file.print(hum1, 1);
+    file.print("% | S2 Temp: "); file.print(temp2, 1);
+    file.print(" C | S2 Hum: "); file.print(hum2, 1);
     file.println("%");
 
     file.close();
-
     Serial.println("Data indskrevet på SD");
 }
