2b783c0d940bb73269310db1af33e5279d7f9eb4
Neas93 <Neas93@gmail.com>
2025-12-08 14:04:54 +0100

    mic

--- Arduino/Datalogger_sketch/Mic.cpp
+++ Arduino/Datalogger_sketch/Mic.cpp
@@ -1,1 +1,12 @@
-#include "Mic.h"
+/*#include "Mic.h"
+#include <Arduino.h>
+
+MicSensor::MicSensor(int p) : pin(p) {
+    pinMode(pin, INPUT);   t
+}
+
+// Returnerer true, hvis der registreres lydaktivitet
+bool MicSensor::aktivitet() {
+    return digitalRead(pin) == HIGH;
+}
+*/
--- Arduino/Datalogger_sketch/Mic.h
+++ Arduino/Datalogger_sketch/Mic.h
@@ -1,3 +1,10 @@
-#pragma once
-#include <Arduino.h>
+/*#pragma once
 
+class MicSensor {
+public:
+    int pin;                
+
+    MicSensor(int p);       
+    bool aktivitet();       // Returnerer true hvis der er lyd
+};
+*/
