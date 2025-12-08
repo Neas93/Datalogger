e5a81085ac87bbed344bd2298c31f9ab96d1ea84
Neas93 <Neas93@gmail.com>
2025-12-08 08:42:56 +0100

    Update RTC.cpp

--- Arduino/Datalogger_sketch/RTC.cpp
+++ Arduino/Datalogger_sketch/RTC.cpp
@@ -2,9 +2,9 @@
 
 RTC::RTC()
 {
-    // Constructor - intet nødvendigt
+    // Constructor - står tom da der ikke er nogle parametre
 }
-
+//opstart af RTC-modul
 void RTC::begin()
 {
     Wire.begin();
@@ -14,10 +14,10 @@ void RTC::begin()
 
 String RTC::getTimestamp()
 {
-    clock.forceConversion();       // Opdaterer temperatur/time internt
+    clock.forceConversion();       // Opdaterer intern temperatur/time | sikring af præcision i måling
     dt = clock.getDateTime();      // Hent dato/tid
 
-    char buffer[25];
+    char buffer[25]; // Reserverer 25 bytes til den string der indeholder timestamp
     snprintf(buffer, sizeof(buffer),
              "%04d-%02d-%02d %02d:%02d:%02d",
              dt.year, dt.month, dt.day,
@@ -28,6 +28,6 @@ String RTC::getTimestamp()
 
 float RTC::getTemperature()
 {
-    clock.forceConversion();       // Opdatering
+    clock.forceConversion();       // Opdatering af tid og temp internt på modul | sikring af præcision i måling
     return clock.readTemperature();
 }
